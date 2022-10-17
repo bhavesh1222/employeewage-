8a51db7233353c03ed4e7e813d828b507cb66825
Bhavesh Bhadane Patil <113571629+bhavesh1222@users.noreply.github.com>
2022-10-17 22:27:25 +0530

    employee wage

---
+++ ew.cs
@@ -0,0 +1,25 @@
+﻿namespace Employeewage
+{
+    public static class Employeecheck
+    {
+      public static void checkattendence()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOR = 20;
+
+            int emphrs = 0;
+            int empwage = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+            if  (empcheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+                emphrs = 8;
+
+            }
+
+
+        }
+    }
+
